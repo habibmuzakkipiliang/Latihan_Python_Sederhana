4a79b2a58e103cc582a62ed7896ffed0773c282f
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-11 21:42:06 +0700

    Add files via upload

---
+++ Profil_Habib_Muzakki.py
@@ -0,0 +1,28 @@
+nama = "Habib Muzakki"
+akrab = "Habib"
+usia = "18 tahun"
+kelas = "12 Agama"
+sekolah = "MAN 2 KOTA SERANG"
+tinggi = "172 cm"
+berat = "60 kg"
+darah = "B +"
+status = "Anak PP"
+ig = "habib_muzakki_piliang"
+tiktok = "habib_muzakki_piliang"
+lomba = "OSN-K Informatika 2025"
+tambah = "SSO, RSCI, IOS Informatika 2025"
+
+
+print ("Nama :", nama)
+print ("Akrab :", akrab)
+print ("Usia :", usia)
+print ("Sekolah :", sekolah)
+print ("Kelas : ", kelas)
+print ("Tinggi :", tinggi)
+print ("Berat :", berat)
+print ("Darah :", darah)
+print ("Status :", status)
+print ("IG :", ig)
+print ("Tiktok :", tiktok)
+print ("Lomba :", lomba)
+print ("Tambah :", tambah)
