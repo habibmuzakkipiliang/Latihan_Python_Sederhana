fd8a126daf29b135d95e6e68376ad13232d54720
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-11 21:47:46 +0700

    Add files via upload

---
+++ Dictionary_Python_2.py
@@ -0,0 +1,19 @@
+contoh = {
+    "nama" : "Habib Muzakki",
+    "kelas" : "12 Agama",
+    "nomor hp" : "0821-1347-7785",
+    "usia" : 18,
+    "tinggi" : 170,
+    "berat" : 57,
+    "total" : 200,
+    "jumlah" : True
+}
+
+print ("Nama :", contoh ["nama"]),
+print ("Kelas :", contoh ["kelas"]),
+print ("Nomor Hp :", contoh ["nomor hp"]),
+print ("Usia :", contoh ["usia"]),
+print ("Tinggi :", contoh ["tinggi"]),
+print ("Berat :", contoh ["berat"]),
+print ("Total :", contoh ["total"]),
+print ("Jumlah :", contoh ["jumlah"]),
---
+++ Dictionary_String_Python.py
@@ -0,0 +1,17 @@
+exam = {
+    "nama" : "Habib Muzakki",
+    "kelas" : "12 Agama",
+    "umur" : "18 tahun",
+    "tinggi" : "172 cm",
+    "berat" : "57 kg",
+    "passion" : ["Coding", "Teknologi", "Digital"],
+    "belajar" : ["HTML", "CSS", "JavaScript Dasar", "Python Dasar"]
+}
+
+print ("Nama : ", exam ["nama"] ),
+print ("Kelas : ", exam ["kelas"]),
+print ("Umur : ", exam ["umur"]),
+print ("Tinggi : ", exam ["tinggi"]),
+print ("Berat : ", exam ["berat"])
+print ("Passion : ", exam ["passion"])
+print ("Belajar : ", exam ["belajar"])
