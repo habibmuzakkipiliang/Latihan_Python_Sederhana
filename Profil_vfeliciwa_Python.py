48ae9c06013e3e6eb2da97535cb55b747bacce1c
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-12 05:38:33 +0700

    Add files via upload

---
+++ Profil_vfeliciwa_Python.py
@@ -0,0 +1,23 @@
+nama = "Felicia Kurniawati"
+akrab = "Feli"
+usia = "22 tahun"
+asal = "Malang"
+agama = "Katolik"
+bisnis = "Pemilik Kafe Cimi Time Malang"
+job = "Konten Kreator"
+hewan = "Bebek Peking"
+ig = "vfeliciwa"
+tiktok = "vfeliciwa"
+youtube = "vfeliciwa"
+
+print ("Nama :", nama)
+print ("Akrab :", akrab)
+print ("Usia :", usia)
+print ("Asal : ", asal)
+print ("Agama :", agama)
+print ("Bisnis :", bisnis)
+print ("Job :", job)
+print ("Hewan :", hewan)
+print ("IG :", ig)
+print ("Tiktok :", tiktok)
+print ("Youtube :", youtube)
