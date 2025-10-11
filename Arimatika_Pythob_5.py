30a820a4a50d8477febbf19f96f5bf7fe5177211
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-11 21:45:36 +0700

    Add files via upload

---
+++ Arimatika_Pythob_5.py
@@ -0,0 +1,15 @@
+# operator logika 
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+
+print (x is y)
+print (x == y)
+
+
+a = 34 
+b = 24
+
+print ("Tambah :", a + b)
+print ("Kurang :", a - b)
+print ("Kali : ", a * b)
---
+++ Aritmatika_Logika_Python.py
@@ -0,0 +1,29 @@
+# Operator Arimatika 
+
+a = 2
+b = 5
+
+print ("Tambah = ", a + b)
+print ("Kurang = ", a - b)
+print ("Kali = ", a * b)
+print ("Pangkat = ", a ** b)
+print ("Modulus = ", a % b)
+print ("Pembagian = ", a / b)
+print ("Pembagian bulat = ", a // b, "\n")
+
+
+# operator Perbandingan dan logika
+
+f = 45
+g = 50
+
+print ("Hasilnya : ", f < g)
+print ("Hasilnya : ", f > g)
+print ("Hasilnya : ", f == g)
+print ("Hasinya : ", f >= g)
+print ("Hasilnya : ", f <= g, "\n")
+
+print (f > g and f < g)
+print (f < g or f > g)
+print (not (f < g))
+print (not (f > g))
