f643de50e17be35593d330715836db46ab7866a8
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-11 21:41:41 +0700

    Add files via upload

---
+++ Objek_Python_Profil_Habib_Muzakki.py
@@ -0,0 +1,35 @@
+oj = {
+    "nama" : "Habib Muzakki",
+    "akrab" :"Habib",
+    "kelas" : "12 Agama",
+    "usia" : "18 tahun",
+    "tinggi" : "172 cm",
+    "berat" : "60 kg",
+    "darah" : "B +",
+    "status" : "Anak PP",
+    "sekolah" : "MAN 2 Kota Serang",
+    "ig" : "habib_muzakki_piliang",
+    "tiktok" : "habib_muzakki_piliang",
+}
+
+print ("Nama :", oj ["nama"])
+
+print ("Akrab :", oj ["akrab"])
+
+print ("Kelas :", oj ["kelas"])
+
+print ("Usia :", oj ["usia"])
+
+print ("Tinggi :", oj ["tinggi"])
+
+print ("Berat :", oj ["berat"])
+
+print ("Darah :", oj ["darah"])
+
+print ("Status :", oj ["status"])
+
+print ("Sekolah :", oj ["sekolah"])
+
+print ("IG :", oj ["ig"])
+
+print ("Tiktok :", oj ["tiktok"])
---
+++ Objek_Python_Profil_vfeliciwa.py
@@ -0,0 +1,35 @@
+obj = { 
+    "nama" : "Felicia Kurniawati",
+    "akrab" : "Feli",
+    "usia" : "22 tahun",
+    "asal" : "Malang",
+    "agama" : "Katolik",
+    "bisnis" : "Pemilik Kafe Cimi Time Malang",
+    "job" : "Konten Kreator",
+    "hewan" : "Bebek Peking",
+    "ig" : "vfeliciwa",
+    "tiktok" : "vfeliciwa",
+    "youtube" : "vfeliciwa",
+} 
+
+print ("Nama :", obj ["nama"])
+
+print ("Akrab :", obj ["akrab"])
+
+print ("Usia :", obj ["usia"])
+
+print ("Asal :", obj ["asal"])
+
+print ("Agama :", obj ["agama"])
+
+print ("Bisnis :", obj ["bisnis"])
+
+print ("Job :", obj ["job"])
+
+print ("Hewan :", obj ["hewan"])
+
+print ("IG :", obj ["ig"])
+
+print ("Tiktok :", obj ["tiktok"])
+
+print ("Youtube :", obj ["youtube"])
