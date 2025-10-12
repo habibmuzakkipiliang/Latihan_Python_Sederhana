cf775c79f8890aa72365447df745884ebdf41c6b
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-12 19:03:32 +0700

    Add files via upload

---
+++ input_hello_world.py
@@ -0,0 +1,8 @@
+# input dan output
+nama = input("Masukkan nama");
+kelas = input("Masukkan kelas");
+bidang = input("Masukkan bidang");
+print ("Halo " + nama + " " + kelas + " " + bidang);
+
+# 
+print ("Hello World")
