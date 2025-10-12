c537fb81df33b31babc448e756edc90c233b610d
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-12 18:37:16 +0700

    Delete input_hello_world.py

--- input_hello_world.py
+++
@@ -1,8 +0,0 @@
-# input dan output
-nama = input("Masukkan nama");
-kelas = input("Masukkan kelas");
-bidang = input("Masukkan bidang");
-print ("Halo" + nama + kelas + bidang);
-
-# 
-print ("Hello World")
