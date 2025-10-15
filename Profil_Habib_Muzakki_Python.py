a99c57166c13c995c0004670000f0dfb1a8d7109
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-15 17:54:39 +0700

    Update Profil_Habib_Muzakki_Python.py

--- Profil_Habib_Muzakki_Python.py
+++ Profil_Habib_Muzakki_Python.py
@@ -1,6 +1,7 @@
 nama = "Habib Muzakki"
 akrab = "Habib"
 usia = "18 tahun"
+lahir = "2007"
 kelas = "12 Agama"
 sekolah = "MAN 2 KOTA SERANG"
 tinggi = "172 cm"
@@ -16,6 +17,7 @@ tambah = "SSO, RSCI, IOS Informatika 2025"
 print ("Nama :", nama)
 print ("Akrab :", akrab)
 print ("Usia :", usia)
+print ("Tahun lahir :", lahir)
 print ("Sekolah :", sekolah)
 print ("Kelas : ", kelas)
 print ("Tinggi :", tinggi)
