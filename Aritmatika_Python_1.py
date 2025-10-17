521b25b57b7bf28b3a14d28821804da1bb636821
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-17 19:51:03 +0700

    Add files via upload

---
+++ Aritmatika_Python_1.py
@@ -0,0 +1,12 @@
+# aritmatika
+
+r = 12 
+t = 2
+
+print ("Tambah : ", r + t) 
+print ("Kurang : ", r - t)
+print ("Kali : ", r * t)
+print ("Pangkat : ", r ** t)
+print ("Pembagian : ", r / t)
+print ("Pembagian bulat : ", r // t)
+print ("Mod")
