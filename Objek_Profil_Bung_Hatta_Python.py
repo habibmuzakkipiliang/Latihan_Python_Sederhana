d161b6abf4418c000bff91710d2a720e3ebef2a6
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-18 12:18:28 +0700

    Add files via upload

---
+++ Objek_Profil_Bung_Hatta_Python.py
@@ -0,0 +1,38 @@
+op = {
+    "nama_asli" : "Mohammad Athar",
+    "akrab" : "Bung Hatta",
+    "lahir" : "19 Agustus 1902",
+    "tempat_lahir" : "Kota Bukittinggi",
+    "asal_provinsi" : "Sumatra Barat",
+    "makam" : "Taman Pemakaman Umum Tanah Kusir",
+    "tanggal_wafat" : "14 Maret 1980",
+    "tempat_wafat" : "Jakarta",
+    "usia" : "77 tahun",
+    "pekerjaan" : "Politikus, Ekonom",
+    "etnis" : "Minangkabau",
+    "klan" : "Piliang",
+}
+
+print ("Nama Asli :", op ["nama_asli"])
+
+print ("Akrab :", op ["akrab"])
+
+print ("Lahir :", op ["lahir"])
+
+print ("Tempat lahir :", op ["tempat_lahir"])
+
+print ("Asal Provinsi :", op ["asal_provinsi"])
+
+print ("Makam :", op ["makam"])
+
+print ("Tanggal wafat :", op ["tanggal_wafat"])
+
+print ("Tempat wafat :", op ["tempat_wafat"])
+
+print ("Usia :", op ["usia"])
+
+print ("Pekerjaan :", op ["pekerjaan"])
+
+print ("Etnis :", op ["etnis"])
+
+print ("Klan :", op ["klan"])
